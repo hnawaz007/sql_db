dd52bb8b1b968f2d0563824d081f3c2f2324b394
hnawaz007 <hnawaz100@gmail.com>
2021-06-05 18:56:19 -0400

    adding sequence to cutomer dim

--- Dimension/tables/Dimension.Customer.sql
+++ Dimension/tables/Dimension.Customer.sql
@@ -27,3 +27,7 @@ CREATE TABLE [Dimension].[Customer](
 End
 GO
 
+---Second Change
+ALTER TABLE [Dimension].[Customer] ADD  CONSTRAINT [DF_Dimension_Customer_Customer_Key]  
+DEFAULT (NEXT VALUE FOR [Sequences].[CustomerKey]) FOR [Customer Key]
+GO
