beff07046c4282808b3bf327c859f5f841811eef
hnawaz007 <hnawaz100@gmail.com>
2021-06-05 13:25:31 -0400

    initial DDL for customer table, dimension schema

---
+++ Dimension/tables/Dimension.Customer.sql
@@ -0,0 +1,29 @@
+USE [WideWorldImportersDW]
+GO
+
+/****** Object:  Table [Dimension].[Customer]    Script Date: 6/4/2021 10:49:08 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+IF  NOT EXISTS (SELECT * FROM sys.objects 
+WHERE object_id = OBJECT_ID(N'[Dimension].[Customer]') AND type in (N'U'))
+
+BEGIN
+CREATE TABLE [Dimension].[Customer](
+	[Customer Key] [int] NOT NULL,
+	[WWI Customer ID] [int] NOT NULL,
+	[Customer] [nvarchar](100) NOT NULL,
+	[Bill To Customer] [nvarchar](100) NOT NULL,
+	[Category] [nvarchar](50) NOT NULL,
+	[Buying Group] [nvarchar](50) NOT NULL,
+	[Primary Contact] [nvarchar](50) NOT NULL,
+	[Postal Code] [nvarchar](10) NOT NULL,
+	[Valid From] [datetime2](7) NOT NULL,
+	[Valid To] [datetime2](7) NOT NULL,
+	[Lineage Key] [int] NOT NULL
+) ON [Primary]
+End
+GO
+
